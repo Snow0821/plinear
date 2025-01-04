db52e951ed48453a02f76d28141da4bb8cd4111a
Snow0821 <sosaror@gmail.com>
2025-01-04 15:46:41 +0900

    plienar beautified

--- plinear/btnn/linear.py
+++ plinear/btnn/linear.py
@@ -7,27 +7,23 @@ from plinear.core import posNet
 class Linear(nn.Module):
     def __init__(self, x, y):
         super(Linear, self).__init__()
-        self.real_pos = nn.Linear(x, y)
-        self.real_neg = nn.Linear(x, y)
+        self.pr = nn.Linear(x, y)
+        self.nr = nn.Linear(x, y)
 
         torch.nn.init.uniform_(self.real_pos.weight, -1, 1)
         torch.nn.init.uniform_(self.real_neg.weight, -1, 1)
 
     def forward(self, x):
-        w_pos = self.real_pos.weight
-        w_neg = self.real_neg.weight
-        tern_pos = posNet(w_pos)
-        tern_neg = posNet(w_neg)
+        pr = self.pr.weight
+        nr = self.nr.weight
+        qpr = posNet(pr)
+        qnr = posNet(nr)
 
         # Apply quantization using posNet with detach
-        tern_pos = tern_pos - w_pos.detach() + w_pos
-        tern_neg = tern_neg - w_neg.detach() + w_neg
+        qpr = qpr - pr.detach() + pr
+        qnr = qnr - nr.detach() + nr
 
         # Compute linear transformations
-        y_pos = F.linear(x, tern_pos)
-        y_neg = F.linear(x, tern_neg)
+        yr = F.linear(x, qpr) - F.linear(x, qnr)
 
-        # Combine positive and negative parts
-        y = y_pos - y_neg
-
-        return y
+        return yr
